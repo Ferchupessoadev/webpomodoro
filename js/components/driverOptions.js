89641dd36f9777acedc71064c8d17f1c86629772
Fernando matías pessoa <fernandomatiaspessoa471@gmail.com>
2023-05-02 15:23:05 -0300

    creando Todo list Js

---
+++ js/components/driverOptions.js
@@ -0,0 +1,31 @@
+class driverOptionsTodoList {
+    constructor() {
+        this.optionsResponsives = document.querySelectorAll(".options-responsive");
+        this.optionsResponsives.forEach(optionsResponsive => {
+            optionsResponsive.firstElementChild.addEventListener("click",e => this.toggleOptions(e.target))
+        });
+        this.optionsResponsiveIsVisible = false;
+        this.containerListTodo = document.querySelector(".card-task")
+        this.containerListTodo.addEventListener("click",e => {
+            if(e.target.className === "rotate-arrow-completed"|| e.target.className === "p-tasks p-future" || e.target.className === "p-tasks p-past" || e.target.className === "p-tasks p-today" || e.target.className === "p-tasks p-completed") {
+                (e.target.className === "rotate-arrow-completed") ? e.target.parentElement.parentElement.parentElement.children[2].classList.toggle("toggle-section") : e.target.parentElement.parentElement.children[2].classList.toggle("toggle-section");
+            }
+        })
+    }
+
+    toggleOptions(optionsResponsive) {
+        if (!this.optionsResponsiveIsVisible){
+            this.optionsResponsiveIsVisible = true;
+            optionsResponsive.parentElement.lastElementChild.style.display = "flex";
+            optionsResponsive.parentElement.style.background = "#000";
+        } else {
+            this.optionsResponsiveIsVisible = false;
+            optionsResponsive.parentElement.lastElementChild.style.display = "none";
+            optionsResponsive.parentElement.style.background = "transparent";
+        }
+    }
+
+    
+}
+
+export default driverOptionsTodoList;
--- js/todo-list.js
+++ js/todo-list.js
@@ -1,10 +1,32 @@
 "use strict";
+import DriverOptionsTodoList from './components/driverOptions.js'
+
 class todoList {
     constructor() {
+		this.driverOptionsTodoList = new DriverOptionsTodoList()
+		this.body = document.querySelector(".body")
 		this.addTaskBtn = document.querySelector(".nav-task__button");
-		this.addTaskBtn.addEventListener("click",() => this.OpenTaskCreationModal());
+		this.addTaskBtn.addEventListener("click",() => this.openTaskCreationModal());
+		this.btnToCloseTheTaskCreationModal = document.querySelector(".container-btn-task__button-close")
+		this.btnToCloseTheTaskCreationModal.addEventListener("click", e => this.closeTaskCreationModal())
+		this.createTaskBtn = document.getElementById("btn-create-task");
+		this.createTaskBtn.addEventListener("click", e => this.validateInputToCreateTheTask())
+		this.titleInput = document.querySelector(".task-create__input-title");
+		this.descriptionInput = document.querySelector("task-create__input-description");
+		this.importanceAccordingToColor = document.getElementById("importance-according-to-color");
+		this.dateOfTask = document.getElementById("date-of-the-task");
+		this.taskOfThePresent = [];
+		this.taskOfThePast = [];
+		this.taskOfTheFuture = [];
+		this.completedTask = [];
+		this.divContainerColors;
+		this.colorOfTheTask = "red"; //default is red
+		this.divContainerColors = null;
+		this.divContainerInfoColor = null;
+		this.arrowColors = document.querySelector(".arrow-down-title")
 		this.btnColorsImportantTask = document.querySelector(".container-circulo");
-		this.btnColorsImportantTask.addEventListener("click",()=>this.showModalColors());
+		this.modalColorsIsVisible = false;
+		this.btnColorsImportantTask.addEventListener("click",()=> (!this.modalColorsIsVisible) ? (this.modalColorsIsVisible = true ,this.showOrCloseModalOfColors()) :(this.modalColorsIsVisible = false ,this.arrowColors.style.transform = "rotate(90deg)", this.divContainerInfoColor.remove()));
 		if (window.innerWidth <= 610) {
 			this.addTaskBtn.innerHTML = "<p>+</p>";
 		}
@@ -17,41 +39,119 @@ class todoList {
 		this.arrowRight.addEventListener("click",()=>{
 			this.sliderCategorias.scrollBy(110,0)
 		})
+
 		this.arrowLeft.addEventListener("click",()=>{
 			this.sliderCategorias.scrollBy(-110,0)
 		})
     }
 
-    OpenTaskCreationModal() {
-		console.log("modal de creacion de tarea.");	
-		
+    openTaskCreationModal() {
+		this.body.style.overflowY = "hidden"
+		const modalForCreatingTask = document.querySelector(".container-task-create")
+		modalForCreatingTask.style.display = "flex";
+		const fecha = new Date()
+		this.dateOfTask.value = fecha.toJSON().slice(0,10);
+		console.log(fecha.getHours()+"-"+fecha.getMinutes())
     }
 
-	/*<div class="container-colors">
-		<div class="triangulo"></div>
-		<div class="circulo-important"></div>
-		<div class="circulo-important"></div>
-		<div class="circulo-important"></div>
-		<div class="circulo-important"></div>
-		<div class="circulo-important"></div>
-		<div class="circulo-important"></div>
-		<div class="circulo-important"></div>
-		<div class="circulo-important"></div>
-		<div class="circulo-important"></div>
-	</div> */
-	showModalColors() {
-		let colors = ["red","orange","rebeccapurple","yellow","brown","blue"]
+	closeTaskCreationModal() {
+		const modalForCreatingTask = document.querySelector(".container-task-create");
+		modalForCreatingTask.style.display = "none"
+		this.body.style.overflowY = "auto"
+		if (this.divContainerInfoColor) this.divContainerInfoColor.remove()
+		this.modalColorsIsVisible = false;
+		this.arrowColors.style.transform = "rotate(90deg)"
+		this.btnColorsImportantTask.firstElementChild.style.background = "red";
+	}
+
+	
+
+	showColorWhenHoveringOver(colorCircle) {
+		if(colorCircle.className == "circulo-important") {
+			const divContainerInfoColor = document.createElement("DIV");
+			divContainerInfoColor.classList.add("container-color-info")
+			divContainerInfoColor.setAttribute("id","container-info-color")
+			const divInfoColor = document.createElement("DIV");
+			divInfoColor.classList.add("info-color")
+			const infoText = document.createElement("P");
+			infoText.innerHTML = colorCircle.style.background;
+			let leftColors = {
+				red:35,
+				goldenrod:82,
+				orange:60,
+				rebeccapurple:113,
+				yellow:55,
+				brown:55,
+				blue:40,
+				lightblue:70,
+				green:50
+
+			}
+			const leftOfInfo = leftColors[colorCircle.style.background]
+			infoText.classList.add("info-color__p")
+			const triangulo = document.createElement("DIV");
+			triangulo.classList.add("triangulo-info-color")
+			divInfoColor.appendChild(infoText)
+			divInfoColor.appendChild(triangulo)
+			this.infoTextColor = divContainerInfoColor;
+			divContainerInfoColor.appendChild(divInfoColor)
+			divContainerInfoColor.style.left = `${(colorCircle.getBoundingClientRect().left - leftOfInfo)}px`
+			divContainerInfoColor.style.right = `${colorCircle.getBoundingClientRect().right}px`
+			divContainerInfoColor.style.top = `${(colorCircle.getBoundingClientRect().top + 30 + scrollY)}px`
+			divContainerInfoColor.style.bottom = `${colorCircle.getBoundingClientRect().bottom}px`
+			colorCircle.addEventListener("mouseout",(e)=> divContainerInfoColor.remove())
+			this.body.appendChild(divContainerInfoColor)
+		}
+	}
+
+	showOrCloseModalOfColors() {		
+		this.arrowColors.style.transform = "rotate(-90deg)"
+		let colors = ["red","goldenrod","orange","rebeccapurple","yellow","brown","blue","lightblue","green"]
 		const divContainerColors = document.createElement("DIV");
 		divContainerColors.classList.add("container-colors");
-		const fragmento = document.createDocumentFragment();
+		this.divContainerInfoColor = divContainerColors;
+		let fragmento = document.createDocumentFragment();
+		const triangulo = document.createElement("DIV")
+		triangulo.classList.add("triangulo")
+		fragmento.appendChild(triangulo)
 		colors.forEach(color => {
-			const divColor = document.createElement("")
+			const divColor = document.createElement("DIV");
+			divColor.classList.add("circulo-important");
+			divColor.style.background = color;
+			divColor.addEventListener("click",e => {
+				this.btnColorsImportantTask.firstElementChild.style.background = color;
+				this.infoTextColor.remove();
+				this.divContainerColors.remove();
+				this.modalColorsIsVisible = false;
+				this.arrowColors.style.transform = "rotate(90deg)";
+			});
+			fragmento.appendChild(divColor);
 		})
-		this.divContainerColors.style.display = "grid";
-		this.divContainerColors.style.left = `${(this.btnColorsImportantTask.getBoundingClientRect().left) - 60}px`
-		this.divContainerColors.style.right = `${this.btnColorsImportantTask.getBoundingClientRect().right}px`
-		this.divContainerColors.style.top = `${(this.btnColorsImportantTask.getBoundingClientRect().top) + 60}px`
-		this.divContainerColors.style.bottom = `${this.btnColorsImportantTask.getBoundingClientRect().bottom}px`
+		divContainerColors.appendChild(fragmento)
+		divContainerColors.style.display = "grid";
+		divContainerColors.style.left = `${(this.btnColorsImportantTask.getBoundingClientRect().left) - 69.7}px`
+		divContainerColors.style.right = `${this.btnColorsImportantTask.getBoundingClientRect().right}px`
+		divContainerColors.style.top = `${(this.btnColorsImportantTask.getBoundingClientRect().top) + 60 + scrollY}px`
+		divContainerColors.style.bottom = `${this.btnColorsImportantTask.getBoundingClientRect().bottom}px`
+		this.divContainerColors = divContainerColors
+		this.divContainerColors.addEventListener("mouseover", e => this.showColorWhenHoveringOver(e.target))
+		this.body.appendChild(divContainerColors)
+	}
+
+	validateInputToCreateTheTask() {
+		
+		if(this.titleInput.value == "") {
+			this.titleInput.placeholder = "Campo obligatorio";
+			this.titleInput.classList.add("color-placeholder-red")
+			this.titleInput.addEventListener("click",() => {
+				this.titleInput.placeholder = "Añadir un titulo";
+				this.titleInput.classList.remove("color-placeholder-red")
+			})
+		}
+	}
+
+	createNewTask(newTask) {
+		
 	}
 }
 
